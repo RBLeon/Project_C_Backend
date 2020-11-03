ab0089f904bfff4689aea64f49b6ddb640b15bd8
RBLeon <57948143+RBLeon@users.noreply.github.com>
2020-11-03 16:48:06 +0100

    Added Hiberate, completed spring setup and have hibernate functional

---
+++ src/main/java/nl/CrsX/model/Car.java
@@ -0,0 +1,76 @@
+package nl.CrsX.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "Car")
+public class Car {
+
+    @Id //tell hibernate this is a PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String model;
+    private String location;
+    private Integer mileage;
+    private Integer carAge;
+    private Integer doors;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Integer mileage) {
+        this.mileage = mileage;
+    }
+
+    public Integer getCarAge() {
+        return carAge;
+    }
+
+    public void setCarAge(Integer carAge) {
+        this.carAge = carAge;
+    }
+
+    public Integer getDoors() {
+        return doors;
+    }
+
+    public void setDoors(Integer doors) {
+        this.doors = doors;
+    }
+}
